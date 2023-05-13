b057770368c78944014a43dc44709f05ccd8cc07
Jonathan Tseng <me@tsengjonathan.com>
2023-05-13 16:09:38 +0800

    chore: scaffold

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,36 @@
 import '@/styles/globals.css'
+import '@rainbow-me/rainbowkit/styles.css';
+
+import {
+  getDefaultWallets,
+  RainbowKitProvider
+} from '@rainbow-me/rainbowkit';
+import { configureChains, createClient, WagmiConfig } from 'wagmi';
+import { mainnet, goerli } from 'wagmi/chains';
+import { publicProvider } from 'wagmi/providers/public';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const { chains, provider, webSocketProvider } = configureChains(
+    [mainnet, goerli],
+    [publicProvider()]
+  );
+  
+  const { connectors } = getDefaultWallets({
+    appName: 'My RainbowKit App',
+    chains
+  });
+  
+  const wagmiConfig = createClient({
+    connectors,
+    provider,
+    webSocketProvider,
+  });
+
+  return (
+    <WagmiConfig client={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  )
 }
